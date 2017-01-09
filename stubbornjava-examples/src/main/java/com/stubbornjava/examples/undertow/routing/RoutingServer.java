e69ae40f2a50443c43e3bb9649fb6ffc9c37e127
Bill O'Neil <bill@dartalley.com>
2017-01-08 19:03:55 -0500

    Minor comment changes

--- stubbornjava-examples/src/main/java/com/stubbornjava/examples/undertow/routing/RoutingServer.java
+++ stubbornjava-examples/src/main/java/com/stubbornjava/examples/undertow/routing/RoutingServer.java
@@ -12,6 +12,7 @@ import io.undertow.server.RoutingHandler;
 public class RoutingServer {
     private static final Logger logger = LoggerFactory.getLogger(RoutingServer.class);
 
+    // {{start:routing}}
     /*
      * Utilize Undertow's RoutingHandler for HTTP Verb / path routing.
      * We can utilize the fallback handler for the not found handler.
@@ -25,7 +26,7 @@ public class RoutingServer {
      * fallbackHandler.
      *
      * How you organize your routes is entirely up to you and Undertow is very flexible
-     * in this regard. StubbornJava tends to use a single file for routing even if it gets
+     * in this regard. We tend to use a single routing file per service even if it gets
      * to 100+ routes. It is very convenient for searching code and trying to track down the
      * code that is executed for a given route. Also using the method reference style there
      * should be almost no logic in the file so readability isn't a major concern.
@@ -44,12 +45,14 @@ public class RoutingServer {
         .get("/myRoute", RoutingHandlers.constantStringHandler("GET - My Route"))
         .post("/myRoute", RoutingHandlers.constantStringHandler("POST - My Route"))
         .get("/myOtherRoute", RoutingHandlers.constantStringHandler("GET - My Other Route"))
-        //.get("/myRoutePrefix*", RoutingHandlers.constantStringHandler("GET - My Prefixed Route"))
-
+        // Wildcards and RoutingHandler had some bugs before version 1.4.8.Final
+        .get("/myRoutePrefix*", RoutingHandlers.constantStringHandler("GET - My Prefixed Route"))
         // Pass a handler as a method reference.
         .setFallbackHandler(RoutingHandlers::notFoundHandler)
     ;
+    // {{end:routing}}
 
+    // {{start:server}}
     public static void main(String[] args) {
         /*
          * You caught us! We changed our mind and decided to add a small abstraction.
@@ -62,4 +65,5 @@ public class RoutingServer {
         Undertow undertow = server.getUndertow();
         server.start();
     }
+    // {{end:server}}
 }
