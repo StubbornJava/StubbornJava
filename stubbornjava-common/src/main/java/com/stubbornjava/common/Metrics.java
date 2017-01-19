10f1a26486e0664d5bc5b54cbdd8f363dc5158dc
Bill O'Neil <bill@dartalley.com>
2017-01-18 22:29:00 -0500

    Improve metrics

--- stubbornjava-common/src/main/java/com/stubbornjava/common/Metrics.java
+++ stubbornjava-common/src/main/java/com/stubbornjava/common/Metrics.java
@@ -2,17 +2,42 @@ package com.stubbornjava.common;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.LoggerFactory;
 
 import com.codahale.metrics.Meter;
 import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Timer;
+import com.codahale.metrics.jvm.CachedThreadStatesGaugeSet;
+import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
+import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
+import com.codahale.metrics.logback.InstrumentedAppender;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+// {{start:metrics}}
 public class Metrics {
-    private static final MetricRegistry registry;
+    /*
+     * Use a concurrent map to cache metrics we generate on the fly.
+     * For example we generate status code metrics on the fly.
+     */
     private static final Map<String, Metric> metricCache = new ConcurrentHashMap<>();
+    private static final MetricRegistry registry;
     static {
         registry = new MetricRegistry();
+        registry.register("gc", new GarbageCollectorMetricSet());
+        registry.register("threads", new CachedThreadStatesGaugeSet(10, TimeUnit.SECONDS));
+        registry.register("memory", new MemoryUsageGaugeSet());
+
+        // Logback metrics
+        final LoggerContext factory = (LoggerContext) LoggerFactory.getILoggerFactory();
+        final Logger root = factory.getLogger(Logger.ROOT_LOGGER_NAME);
+        final InstrumentedAppender metrics = new InstrumentedAppender(registry);
+        metrics.setContext(root.getLoggerContext());
+        metrics.start();
+        root.addAppender(metrics);
 
         // Register reporters here.
     }
@@ -24,9 +49,9 @@ public class Metrics {
     public static Timer timer(String first, String... keys) {
         String key = MetricRegistry.name(first, keys);
         return (Timer) metricCache.computeIfAbsent(key, (String metricName) -> {
-                Timer metric = new Timer();
-                registry.register(metricName, metric);
-                return metric;
+            Timer metric = new Timer();
+            registry.register(metricName, metric);
+            return metric;
         });
     }
 
@@ -39,3 +64,4 @@ public class Metrics {
         });
     }
 }
+// {{end:metrics}}
