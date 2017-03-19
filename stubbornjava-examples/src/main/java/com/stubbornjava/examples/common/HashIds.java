192de48a855f8b3bcf178a3093bbf83d9e094ba3
Bill O'Neil <bill@dartalley.com>
2017-03-19 12:46:05 -0400

    Added hash ids

---
+++ stubbornjava-examples/src/main/java/com/stubbornjava/examples/common/HashIds.java
@@ -0,0 +1,24 @@
+package com.stubbornjava.examples.common;
+
+import org.hashids.Hashids;
+
+public class HashIds {
+    /*
+     *  The salt is important so that your ids cannot be guessed.
+     *  If you used a default hash an attacker could generate all possible ids
+     *  which defeats the purpose of obfuscating the ids and making them non sequential.
+     */
+    private static final Hashids hashids = new Hashids("Your Salt Here", 3);
+
+    public static String encode(long... ids) {
+        return hashids.encode(ids);
+    }
+
+    public static long[] decodeArray(String hash) {
+        return hashids.decode(hash);
+    }
+
+    public static long decode(String hash) {
+        return hashids.decode(hash)[0];
+    }
+}
--- stubbornjava-examples/src/main/java/com/stubbornjava/examples/undertow/parameters/ParametersServer.java
+++ stubbornjava-examples/src/main/java/com/stubbornjava/examples/undertow/parameters/ParametersServer.java
@@ -1,14 +1,21 @@
 package com.stubbornjava.examples.undertow.parameters;
 
+import java.util.stream.LongStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.common.base.Strings;
 import com.stubbornjava.common.undertow.Exchange;
 import com.stubbornjava.common.undertow.SimpleServer;
+import com.stubbornjava.examples.common.HashIds;
 
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.server.RoutingHandler;
 
 public class ParametersServer {
+    private static final Logger log = LoggerFactory.getLogger(ParametersServer.class);
 
     // {{start:handlers}}
     private static void queryParam(HttpServerExchange exchange) {
@@ -24,15 +31,46 @@ public class ParametersServer {
     }
     // {{end:handlers}}
 
+    // {{start:obfuscatedRoute}}
+    private static void obfuscatedId(HttpServerExchange exchange) {
+        // Using the above helper
+        Long userId = ParametersServer.userId(exchange);
+        // This is just to show the raw value.
+        String rawUserIdParam = Exchange.pathParams()
+                                        .pathParam(exchange, "userId")
+                                        .orElse(null);
+        Exchange.body().sendText(exchange, "UserId: " + userId + " hashed: " + rawUserIdParam);
+    }
+    // {{end:obfuscatedRoute}}
+
+    // {{start:obfuscated}}
+    /*
+     * It's useful to create helpers for params you know you will use over and over.
+     * It will reduce boilerpalte, typos, and ensure you use common defaults.
+     * You could even add validation at this step if you wanted.
+     */
+    private static Long userId(HttpServerExchange exchange) {
+        return Exchange.pathParams()
+                       .pathParam(exchange, "userId")
+                       .map(HashIds::decode)
+                       .orElse(null);
+    }
+    // {{end:obfuscated}}
+
     // {{start:routes}}
     private static final HttpHandler ROUTES = new RoutingHandler()
         .get("/hello", ParametersServer::queryParam)
         .get("/hello/{name}/{num}", ParametersServer::pathParam)
+        .get("/users/{userId}", ParametersServer::obfuscatedId)
     ;
     // {{end:routes}}
 
     // {{start:server}}
     public static void main(String[] args) {
+        // Just some examples for obfuscated parameters.
+        LongStream.range(100_000_000, 100_000_003).forEach( id -> {
+           log.debug("id: " + id + " hashed: " + HashIds.encode(id));
+        });
         SimpleServer server = SimpleServer.simpleServer(ROUTES);
         server.start();
     }
