70de7ab00ceea5b5e5f2b81cca4225ebd9a6834f
Bill O'Neil <bill@dartalley.com>
2017-05-17 11:10:25 -0400

    Fix deploy script

--- stubbornjava-webapp/deploy.sh
+++ stubbornjava-webapp/deploy.sh
@@ -3,5 +3,5 @@
 # Ideally it would keep versioned jars and symlink the current version for easy rollbacks
 # Another option is to use AWS code build / deploy eventually
 cd .. && gradle clean shadowJar &&
-ansible -i ansible/hosts stubbornjava -m copy -a "src=build/libs/stubbornjava-all.jar dest=~/" &&
-ansible -i ansible/hosts stubbornjava -m command -a "supervisorctl restart all"
+ansible -i ../stubbornjava/ansible/hosts stubbornjava -m copy -a "src=stubbornjava-webapp/build/libs/stubbornjava-all.jar dest=~/" &&
+ansible -i ../stubbornjava/ansible/hosts stubbornjava -m command -a "supervisorctl restart all"
