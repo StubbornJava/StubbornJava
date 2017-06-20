741e427e9f5057b6f20507024bbf41b72e0f2589
Bill O'Neil <bill@dartalley.com>
2017-06-20 00:03:20 -0400

    Add example images

--- stubbornjava-webapp/ui/src/common/common.js
+++ stubbornjava-webapp/ui/src/common/common.js
@@ -10,3 +10,9 @@ import '../widgets/widgets.js';
 import '../pages/pages.scss';
 
 import '../images/favicon.ico';
+
+// example images
+import '../images/examples/error.png';
+import '../images/examples/hello.png';
+import '../images/examples/homepage.png';
+import '../images/examples/notfound.png';
