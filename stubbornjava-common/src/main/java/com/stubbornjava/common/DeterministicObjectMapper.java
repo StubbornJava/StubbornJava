ae65b764bfd53136fecbc7e58f10d97c80fba166
Bill O'Neil <bill@dartalley.com>
2017-09-21 00:12:13 -0400

    Added a semi deterministic object mapper

---
+++ stubbornjava-common/src/main/java/com/stubbornjava/common/DeterministicObjectMapper.java
@@ -0,0 +1,157 @@
+package com.stubbornjava.common;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+import java.util.Objects;
+
+import org.jooq.lambda.Seq;
+
+import com.fasterxml.jackson.databind.BeanProperty;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.StdDelegatingSerializer;
+import com.fasterxml.jackson.databind.util.Converter;
+import com.fasterxml.jackson.databind.util.StdConverter;
+
+// {{start:DeterministicObjectMapper}}
+public class DeterministicObjectMapper {
+
+    private DeterministicObjectMapper() { }
+
+    public static ObjectMapper create(ObjectMapper original, CustomComparators customComparators) {
+        ObjectMapper mapper = original.copy()
+            .configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true)
+            .configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true);
+
+        /*
+         *  Get the original instance of the SerializerProvider before we add our custom module.
+         *  Our Collection Delegating code does not call itself.
+         */
+        SerializerProvider serializers = mapper.getSerializerProviderInstance();
+
+        // This module is reponsible for replacing non-deterministic objects
+        // with deterministic ones. Example convert Set to a sorted List.
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Collection.class,
+             new CustomDelegatingSerializerProvider(serializers, new CollectionToSortedListConverter(customComparators))
+        );
+        mapper.registerModule(module);
+        return mapper;
+    }
+
+    /*
+     * We need this class to delegate to the original SerializerProvider
+     * before we added our module to it. If we have a Collection -> Collection converter
+     * it delegates to itself and infinite loops until the stack overflows.
+     */
+    private static class CustomDelegatingSerializerProvider extends StdDelegatingSerializer
+    {
+        private final SerializerProvider serializerProvider;
+
+        private CustomDelegatingSerializerProvider(SerializerProvider serializerProvider,
+                                                   Converter<?, ?> converter)
+        {
+            super(converter);
+            this.serializerProvider = serializerProvider;
+        }
+
+        @Override
+        protected StdDelegatingSerializer withDelegate(Converter<Object,?> converter,
+                                                       JavaType delegateType, JsonSerializer<?> delegateSerializer)
+        {
+            return new StdDelegatingSerializer(converter, delegateType, delegateSerializer);
+        }
+
+        /*
+         *  If we do not override this method to delegate to the original
+         *  serializerProvider we get a stack overflow exception because it recursively
+         *  calls itself. Basically we are hijacking the Collection serializer to first
+         *  sort the list then delegate it back to the original serializer.
+         */
+        @Override
+        public JsonSerializer<?> createContextual(SerializerProvider provider, BeanProperty property)
+                throws JsonMappingException
+        {
+            return super.createContextual(serializerProvider, property);
+        }
+    }
+
+    private static class CollectionToSortedListConverter extends StdConverter<Collection<?>, Collection<?>>
+    {
+        private final CustomComparators customComparators;
+
+        public CollectionToSortedListConverter(CustomComparators customComparators) {
+            this.customComparators = customComparators;
+        }
+        @Override
+        public Collection<? extends Object> convert(Collection<?> value)
+        {
+            if (value == null || value.isEmpty())
+            {
+                return Collections.emptyList();
+            }
+
+            /**
+             * Sort all elements by class first, then by our custom comparator.
+             * If the collection is heterogeneous or has anonymous classes its useful
+             * to first sort by the class name then by the comparator. We don't care
+             * about that actual sort order, just that it is deterministic.
+             */
+            Comparator<Object> comparator = Comparator.comparing(x -> x.getClass().getName())
+                                                      .thenComparing(customComparators::compare);
+            Collection<? extends Object> filtered = Seq.seq(value)
+                                                       .filter(Objects::nonNull)
+                                                       .sorted(comparator)
+                                                       .toList();
+            if (filtered.isEmpty())
+            {
+                return Collections.emptyList();
+            }
+
+            return filtered;
+        }
+    }
+
+    public static class CustomComparators {
+        private final LinkedHashMap<Class<?>, Comparator<? extends Object>> customComparators;
+
+        public CustomComparators() {
+            customComparators = new LinkedHashMap<>();
+        }
+
+        public <T> void addConverter(Class<T> clazz, Comparator<?> comparator) {
+            customComparators.put(clazz, comparator);
+        }
+
+        @SuppressWarnings({ "unchecked", "rawtypes" })
+        public int compare(Object first, Object second) {
+            // If the object is comparable use its comparator
+            if (first instanceof Comparable) {
+                return ((Comparable) first).compareTo(second);
+            }
+
+            // If the object is not comparable try a custom supplied comparator
+            for (Entry<Class<?>, Comparator<?>> entry : customComparators.entrySet()) {
+                Class<?> clazz = entry.getKey();
+                if (first.getClass().isAssignableFrom(clazz)) {
+                    Comparator<Object> comparator = (Comparator<Object>) entry.getValue();
+                    return comparator.compare(first, second);
+                }
+            }
+
+            // we have no way to order the collection so fail hard
+            String message = String.format("Cannot compare object of type %s without a custom comparator", first.getClass().getName());
+            throw new UnsupportedOperationException(message);
+        }
+    }
+}
+// {{end:DeterministicObjectMapper}}
---
+++ stubbornjava-common/src/test/java/com/stubbornjava/common/DeterministicObjectMapperTest.java
@@ -0,0 +1,155 @@
+package com.stubbornjava.common;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.jooq.lambda.Seq;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import com.stubbornjava.common.DeterministicObjectMapper.CustomComparators;
+
+// {{start:DeterministicObjectMapperTest}}
+public class DeterministicObjectMapperTest {
+
+    private ObjectMapper mapper;
+
+    @Before
+    public void setup() {
+        CustomComparators customComparators = new DeterministicObjectMapper.CustomComparators();
+        mapper = DeterministicObjectMapper.create(Json.serializer().mapper(), customComparators);
+    }
+
+    @Test
+    public void testDeterministicSetInts() throws JsonProcessingException
+    {
+        Set<Integer> ints = Sets.newLinkedHashSet(Lists.newArrayList(1, 3, 2));
+        String actual = mapper.writer().writeValueAsString(ints);
+        String expected = "[1,2,3]";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDeterministicSetStrings() throws JsonProcessingException
+    {
+        Set<String> strings = Sets.newLinkedHashSet(Lists.newArrayList("a", "c", "b", "aa", "cc", "bb"));
+        String actual = mapper.writer().writeValueAsString(strings);
+        String expected = "[\"a\",\"aa\",\"b\",\"bb\",\"c\",\"cc\"]";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testHeterogeneousList() throws JsonProcessingException
+    {
+        List<Object> strings = Lists.newArrayList("a", 1, "b", "c", 2);
+        String actual = mapper.writer().writeValueAsString(strings);
+        String expected = "[1,2,\"a\",\"b\",\"c\"]";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDeterministicFieldOrder() throws JsonProcessingException
+    {
+        @SuppressWarnings("unused")
+        Object data = new Object() {
+            public String get1() { return "1"; }
+            public String getC() { return "C"; }
+            public String getA() { return "A"; }
+        };
+
+        String actual = mapper.writer().writeValueAsString(data);
+        String expected = "{\"1\":\"1\",\"a\":\"A\",\"c\":\"C\"}";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDeterministicMapKeyOrder() throws JsonProcessingException
+    {
+        Map<String, String> data = Maps.newLinkedHashMap();
+        data.put("1", "1");
+        data.put("a", "A");
+        data.put("c", "C");
+
+        String actual = mapper.writer().writeValueAsString(data);
+        String expected = "{\"1\":\"1\",\"a\":\"A\",\"c\":\"C\"}";
+        assertEquals(expected, actual);
+    }
+
+    @Test(expected=JsonMappingException.class)
+    public void testCustomComparatorFails() throws JsonProcessingException {
+        Set<MyObject> objects = Seq.of(
+            new MyObject(2),
+            new MyObject(4),
+            new MyObject(3),
+            new MyObject(1)
+        ).toSet(Sets::newHashSet);
+
+        mapper.writer().writeValueAsString(objects);
+    }
+
+    @Test
+    public void testCustomComparatorPasses() throws JsonProcessingException {
+        CustomComparators comparators = new DeterministicObjectMapper.CustomComparators();
+        comparators.addConverter(MyObject.class, Comparator.comparing(MyObject::getX));
+        ObjectMapper customizedComparatorsMapper = DeterministicObjectMapper.create(Json.serializer().mapper(), comparators);
+        Set<MyObject> objects = Seq.of(
+            new MyObject(2),
+            new MyObject(4),
+            new MyObject(3),
+            new MyObject(1)
+        ).toSet();
+
+        String actual = customizedComparatorsMapper.writer().writeValueAsString(objects);
+        String expected = "[{\"x\":1},{\"x\":2},{\"x\":3},{\"x\":4}]";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDeterministicNesting() throws JsonProcessingException
+    {
+        @SuppressWarnings("unused")
+        Object obj = new Object() {
+            public String get1() { return "1"; }
+            public String getC() { return "C"; }
+            public String getA() { return "A"; }
+        };
+
+        Set<Integer> ints = Sets.newLinkedHashSet(Lists.newArrayList(1, 4, 2, 3, 5, 7, 8, 9, 6, 0, 50, 100, 99));
+
+        Map<String, Object> data = Maps.newLinkedHashMap();
+        data.put("obj", obj);
+        data.put("c", "C");
+        data.put("ints", ints);
+
+        String actual = mapper.writer().writeValueAsString(data);
+        String expected = "{" +
+                            "\"c\":\"C\"," +
+                            "\"ints\":[0,1,2,3,4,5,6,7,8,9,50,99,100]," +
+                            "\"obj\":{\"1\":\"1\",\"a\":\"A\",\"c\":\"C\"}" +
+                          "}";
+        assertEquals(expected, actual);
+    }
+
+    private static class MyObject {
+        private final int x;
+
+        public MyObject(int x) {
+            this.x = x;
+        }
+
+        public int getX() {
+            return x;
+        }
+    }
+}
+// {{end:DeterministicObjectMapperTest}}
