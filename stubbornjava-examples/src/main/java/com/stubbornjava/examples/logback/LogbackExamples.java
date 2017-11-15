4e5391f51061986addee9c1ac67d430dc8e35897
Bill O'Neil <bill@dartalley.com>
2017-11-14 23:38:09 -0500

    SLF4J more examples

--- stubbornjava-examples/src/main/java/com/stubbornjava/examples/logback/LogbackExamples.java
+++ stubbornjava-examples/src/main/java/com/stubbornjava/examples/logback/LogbackExamples.java
@@ -3,8 +3,9 @@ package com.stubbornjava.examples.logback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// {{start:logback}}
+
 public class LogbackExamples {
+    // {{start:slf4j}}
     /*
      *  Loggers are thread safe so it is okay to make them static.
      *  Sometimes you may want to pass instances though, it's up to you.
@@ -12,8 +13,7 @@ public class LogbackExamples {
     private static final Logger logger = LoggerFactory.getLogger(LogbackExamples.class);
     private static final Logger secretLogger = LoggerFactory.getLogger("com.stubbornjava.secrets.MySecretPasswordClass");
 
-    public static void main(String[] args) {
-
+    public static void logLevels() {
         logger.trace("TRACE");
         logger.info("INFO");
         logger.debug("DEBUG");
@@ -26,6 +26,50 @@ public class LogbackExamples {
         secretLogger.warn("WARN");
         secretLogger.error("ERROR");
     }
+    // {{end:slf4j}}
+
+    // {{start:slf4jFormat}}
+    public static void logFormat() {
+        logger.info("Hello {}", "world");
+
+        for (int i = 0; i < 5; i++) {
+            logger.info("Hello {} i={}", "world", i);
+        }
+    }
+    // {{end:slf4jFormat}}
+
+    // {{start:slf4jConditionalLogging}}
+    public static void conditionalLogging() {
+
+        if (logger.isInfoEnabled()) {
+            Object expensiveCall = null;
+            logger.info("Logger expensive call {}", expensiveCall);
+        }
+
+        if (secretLogger.isInfoEnabled()) {
+            Object expensiveCall = null;
+            logger.info("Secret expensive call {}", expensiveCall);
+        }
+    }
+    // {{end:slf4jConditionalLogging}}
+
+
+    // {{start:slf4jException}}
+    public static void logException() {
+        try {
+            throw new RuntimeException("What happened?");
+        } catch (Exception ex) {
+            logger.warn("Something bad happened", ex);
+            logger.warn("Something bad happened with id: {}", 1, ex);
+        }
+    }
+    // {{end:slf4jException}}
+
+    public static void main(String[] args) {
+        logLevels();
+        logFormat();
+        conditionalLogging();
+        logException();
+    }
 }
-// {{end:logback}}
 
