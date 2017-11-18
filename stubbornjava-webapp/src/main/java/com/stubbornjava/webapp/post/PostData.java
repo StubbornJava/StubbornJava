1aaca18db9f926768df9107d0eda6aa857b5d795
Bill O'Neil <bill@dartalley.com>
2017-11-17 22:51:27 -0500

    more realistic date

--- stubbornjava-webapp/src/main/java/com/stubbornjava/webapp/post/PostData.java
+++ stubbornjava-webapp/src/main/java/com/stubbornjava/webapp/post/PostData.java
@@ -692,8 +692,8 @@ public class PostData {
               .postId(1L)
               .title("Installing Java, supervisord and other service dependencies with Ansible")
               .metaDesc("Example using Ansible to install java8, supervisord, users, groups and file structures in a repeatable way. Configuring AWS EC2 instances with Ansible example.")
-              .dateCreated(LocalDateTime.parse("2017-11-11T01:15:30"))
-              .dateUpdated(LocalDateTime.parse("2017-11-11T01:15:30"))
+              .dateCreated(LocalDateTime.parse("2017-11-17T01:15:30"))
+              .dateUpdated(LocalDateTime.parse("2017-11-17T01:15:30"))
               .javaLibs(Lists.newArrayList())
               .tags(Lists.newArrayList(Tags.Ansible, Tags.Supervisord))
               .gitFileReferences(Lists.newArrayList(
