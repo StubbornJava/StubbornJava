83033969da4f89f169a29ca1947e0f3e20a48969
Sergio <sergio@compstak.com>
2018-03-04 19:06:07 -0500

    create an instance profile and role for use w/ jenkins

--- terraform/iam/iam.tf
+++ terraform/iam/iam.tf
@@ -4,11 +4,6 @@ provider "aws" {
     region = "${var.region}"
 }
 
-resource "aws_iam_user" "jenkins" {
-  name = "jenkins"
-}
-
-
 resource "aws_iam_policy" "jenkins-s3" {
     name        = "jenkins-s3"
     policy      = <<EOF
@@ -29,7 +24,32 @@ resource "aws_iam_policy" "jenkins-s3" {
 EOF
 }
 
-resource "aws_iam_user_policy_attachment" "jenkins-s3" {
-    user       = "${aws_iam_user.jenkins.name}"
+resource "aws_iam_role" "jenkins" {
+  name = "jenkins"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "jenkins" {
+  name  = "jenkins"
+  role = "${aws_iam_role.jenkins.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "jenkins-s3" {
+    role       = "${aws_iam_role.jenkins.name}"
     policy_arn = "${aws_iam_policy.jenkins-s3.arn}"
 }
