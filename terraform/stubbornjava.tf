47528fbe408f215f30812e3d2026f8b2b97696ad
Sergio <sergio@compstak.com>
2018-03-18 18:31:25 -0400

    mount 100GB volume, update /usr/bin/java link

--- terraform/stubbornjava.tf
+++ terraform/stubbornjava.tf
@@ -9,12 +9,31 @@ variable "amis" {
 
 # TODO: import stubbornjava-webapp
 
+resource "aws_volume_attachment" "ebs_jenkins" {
+  device_name = "/dev/sdh"
+  volume_id   = "${aws_ebs_volume.jenkins.id}"
+  instance_id = "${aws_instance.ci.id}"
+
+  skip_destroy = true
+}
+
+resource "aws_ebs_volume" "jenkins" {
+  availability_zone = "us-east-1a"
+  size              = 100
+  type              = "gp2"
+
+  tags {
+      Name = "jenkins"
+      Path = "/var/lib/jenkins"
+  }
+}
+
 resource "aws_instance" "ci" {
   count                       = 1
-  ami                         = "${var.amis["amazon-linux-2017-09"]}"
+  ami                         = "${var.amis["amazon-linux-2017-09.1"]}"
   disable_api_termination     = true
   iam_instance_profile        = "jenkins"
-  instance_type               = "t2.micro"
+  instance_type               = "t2.medium"
   monitoring                  = false
   subnet_id                   = "${element(data.aws_subnet_ids.public.ids, count.index)}"
   key_name                    = "stubbornjava"
@@ -28,7 +47,17 @@ resource "aws_instance" "ci" {
 
   root_block_device {
     volume_type               = "gp2"
-    volume_size               = 20
+    volume_size               = 10
     delete_on_termination     = true
   }
+
+  user_data = <<USER_DATA
+#cloud-config
+bootcmd:
+ - [ cloud-init-per, once, mymkfs, mkfs.ext4, /dev/xvdh ]
+ - [ cloud-init-per, once, createjenkinsdir, mkdir, /var/lib/jenkins ]
+
+mounts:
+ - [ /dev/xvdh, /var/lib/jenkins, "ext4", "defaults,noatime", "0", "0" ]
+USER_DATA
 }
