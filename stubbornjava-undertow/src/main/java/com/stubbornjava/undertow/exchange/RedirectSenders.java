2d98e168794348475f95346d3ba2c9c999250a49
Bill O'Neil <bill@dartalley.com>
2019-03-15 08:15:18 -0400

    Add annotations for RedirectSenders and better redirect content

--- stubbornjava-undertow/src/main/java/com/stubbornjava/undertow/exchange/RedirectSenders.java
+++ stubbornjava-undertow/src/main/java/com/stubbornjava/undertow/exchange/RedirectSenders.java
@@ -6,32 +6,27 @@ import io.undertow.util.StatusCodes;
 
 public interface RedirectSenders {
 
-    /*
-     * Temporary redirect
-     */
+    // {{start:temporary}}
     default void temporary(HttpServerExchange exchange, String location) {
         exchange.setStatusCode(StatusCodes.FOUND);
         exchange.getResponseHeaders().put(Headers.LOCATION, location);
         exchange.endExchange();
     }
+    // {{end:temporary}}
 
-    /*
-     * Permanent redirect
-     */
+    // {{start:permanent}}
     default void permanent(HttpServerExchange exchange, String location) {
         exchange.setStatusCode(StatusCodes.MOVED_PERMANENTLY);
         exchange.getResponseHeaders().put(Headers.LOCATION, location);
         exchange.endExchange();
     }
+    // {{end:permanent}}
 
-    /*
-     * Temporary Redirect to the previous page based on the Referrer header.
-     * This is very useful when you want to redirect to the previous
-     * page after a form submission.
-     */
+    // {{start:referer}}
     default void referer(HttpServerExchange exchange) {
         exchange.setStatusCode(StatusCodes.FOUND);
         exchange.getResponseHeaders().put(Headers.LOCATION, exchange.getRequestHeaders().get(Headers.REFERER, 0));
         exchange.endExchange();
     }
+    // {{end:referer}}
 }
